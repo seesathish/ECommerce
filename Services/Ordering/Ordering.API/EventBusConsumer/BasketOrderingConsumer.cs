0dbaba52611e1039607a4434dbea8e1433515eed
Sathish <seesathish@gmail.com>
2024-09-10 19:48:35 +0530

    Order Service added before integration test.

---
+++ Services/Ordering/Ordering.API/EventBusConsumer/BasketOrderingConsumer.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using EventBus.Messages.Common;
+using MassTransit;
+using MassTransit.Mediator;
+using MediatR;
+using Ordering.Application.Commands;
+
+
+namespace Ordering.API.EventBusConsumer
+{
+    public class BasketOrderingConsumer : IConsumer<BasketCheckoutEvent>
+    {
+        private readonly MediatR.IMediator _mediator;
+        private readonly IMapper _mapper;
+        private readonly ILogger<BasketOrderingConsumer> _logger;
+
+        public BasketOrderingConsumer(MediatR.IMediator mediator, IMapper mapper, ILogger<BasketOrderingConsumer> logger)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        {
+            using var scope = _logger.BeginScope("Consuming Basket Checkout Event for {correlationId}", context.Message.CorrelationId);
+            var cmd = _mapper.Map<CheckoutOrderCommand>(context.Message);
+            var result = await _mediator.Send(cmd);
+            _logger.LogInformation("Basket Checkout Event completed!!!");
+        }
+    }
+}
