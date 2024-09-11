9b5926f18178ebcbb88d773fa8f98cca0fd12bfd
Sathish <seesathish@gmail.com>
2024-09-11 15:46:25 +0530

    Mass transigt implemented.

--- Services/Basket/BAsket.API/Controllers/BasketController.cs
+++ Services/Basket/BAsket.API/Controllers/BasketController.cs
@@ -5,8 +5,8 @@ using Basket.Application.Mappers;
 using Basket.Application.Queries;
 using Basket.Application.Responses;
 using Basket.Core.Entities;
-//using EventBus.Messages.Common;
-//using MassTransit;
+using EventBus.Messages.Common;
+using MassTransit;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
@@ -18,14 +18,14 @@ namespace Basket.API.Controllers
     {
         public readonly IMediator _mediator;
 
-        //private readonly IPublishEndpoint _publishEndpoint;
-        //private readonly ILogger<BasketController> _logger;
+        private readonly IPublishEndpoint _publishEndpoint;
+        private readonly ILogger<BasketController> _logger;
 
-        public BasketController(IMediator mediator)
+        public BasketController(IMediator mediator, IPublishEndpoint publishEndpoint, ILogger<BasketController> logger)
         {
             _mediator = mediator;
-            //_publishEndpoint = publishEndpoint;
-            //_logger = logger;
+            _publishEndpoint = publishEndpoint;
+            _logger = logger;
         }
 
         [HttpGet]
@@ -55,28 +55,28 @@ namespace Basket.API.Controllers
             return Ok(await _mediator.Send(cmd));
         }
 
-        //[Route("[action]")]
-        //[HttpPost]
-        //[ProducesResponseType((int)HttpStatusCode.Accepted)]
-        //[ProducesResponseType((int)HttpStatusCode.BadRequest)]
-        //public async Task<IActionResult> Checkout([FromBody] BasketCheckout basketCheckout)
-        //{
-        //    //Get the existing basket with username
-        //    var query = new GetBasketByUserNameQuery(basketCheckout.UserName);
-        //    var basket = await _mediator.Send(query);
-        //    if (basket == null)
-        //    {
-        //        return BadRequest();
-        //    }
+        [Route("[action]")]
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.Accepted)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> Checkout([FromBody] BasketCheckout basketCheckout)
+        {
+            //Get the existing basket with username
+            var query = new GetBasketByUserNameQuery(basketCheckout.UserName);
+            var basket = await _mediator.Send(query);
+            if (basket == null)
+            {
+                return BadRequest();
+            }
 
-        //    var eventMsg = BasketMapper.Mapper.Map<BasketCheckoutEvent>(basketCheckout);
-        //    eventMsg.TotalPrice = basket.TotalPrice;
-        //    await _publishEndpoint.Publish(eventMsg);
-        //    _logger.LogInformation($"Basket Published for {basket.UserName}");
-        //    //remove the basket
-        //    var deleteCmd = new DeleteBasketByUserNameCommand(basketCheckout.UserName);
-        //    await _mediator.Send(deleteCmd);
-        //    return Accepted();
-        //}
+            var eventMsg = BasketMapper.Mapper.Map<BasketCheckoutEvent>(basketCheckout);
+            eventMsg.TotalPrice = basket.TotalPrice;
+            await _publishEndpoint.Publish(eventMsg);
+            _logger.LogInformation($"Basket Published for {basket.UserName}");
+            //remove the basket
+            var deleteCmd = new DeleteBasketByUserNameCommand(basketCheckout.UserName);
+            await _mediator.Send(deleteCmd);
+            return Accepted();
+        }
     }
 }
